68f635fc3778aeedec0abd375ea4b312989168ef
chihway <chihway@slac.stanford.edu>
2013-04-02 10:35:16 -0700

    add rotation

--- bin/phosim2galsim_image.py
+++ bin/phosim2galsim_image.py
@@ -9,6 +9,7 @@
 # - define pixel
 # - put down star and shift
 # - put down galaxy and shift
+# - choose to do photon shooting or not?
 
 import sys, os, math, numpy, logging, time, galsim
 
@@ -19,10 +20,10 @@ def main(argv):
     logging.basicConfig(format="%(message)s", level=logging.INFO, stream=sys.stdout)
     logger = logging.getLogger("phosim2galsim_test1")
 
-    configfilename='config'   # configuration file
-    starfilename='star'       # star catalog
-    galfilename='gal'         # galaxy catalog
-    outfilename='output.fits'
+    configfilename='data/config'   # configuration file
+    starfilename='data/star'       # star catalog
+    galfilename='data/gal'         # galaxy catalog
+    outfilename='output3.fits'
     if os.path.isfile(outfilename):
       os.remove(outfilename)
 
@@ -70,20 +71,22 @@ def main(argv):
     atm_psf = galsim.Kolmogorov(fwhm = Atmseeing)
     Optpsfsize=Optpsfsize*Airmass**0.6            # arcsec
     Optpsfbeta = 3                    
-    Optpsftrunc = 2.*Optpsfsize                   # arcsec 
+    Optpsftrunc = 10.*Optpsfsize                   # arcsec 
     opt_psf=galsim.Moffat(beta=Optpsfbeta, fwhm=Optpsfsize, trunc=Optpsftrunc)
 
     # Make the pixel:
-    pix = galsim.Pixel(Pixelsize)
+    #pix = galsim.Pixel(Pixelsize)
+    pix=1    # photon-shooting   
 
-    psf=galsim.Convolve([atm_psf, opt_psf, pix])
+    #psf=galsim.Convolve([atm_psf, opt_psf, pix])
+    psf=galsim.Convolve([atm_psf, opt_psf])
 
     galfile=open(galfilename,'r')
     gallines=galfile.readlines()
     galfile.close()
     for k in range(len(gallines)):
     #for k in range(1000):
-      print k 
+      #print k 
       # individual galaxy component parameters
       parameters=gallines[k].split()
       # Initialize the random number generator we will be using for this object:
@@ -127,7 +130,8 @@ def main(argv):
       final.applyShift(dx*Pixelsize,dy*Pixelsize)
 
       # Draw the stamp image
-      stamp = final.draw(dx=Pixelsize)
+      # stamp = final.draw(dx=Pixelsize)
+      stamp=final.drawShoot(n_photons=galFlux, dx=Pixelsize)[0]
 
       # Recenter the stamp at the desired position:
       stamp.setCenter(ix,iy)
@@ -146,7 +150,7 @@ def main(argv):
     starfile.close()
     for k in range(len(starlines)):
     #for k in range(50):
-      print k 
+      #print k 
       # individual galaxy component parameters
       parameters=starlines[k].split()
       # Initialize the random number generator we will be using for this object:
@@ -180,7 +184,8 @@ def main(argv):
       final.applyShift(dx*Pixelsize,dy*Pixelsize)
 
       # Draw the stamp image
-      stamp = final.draw(dx=Pixelsize)
+      stamp=final.drawShoot(n_photons=star, dx=Pixelsize)[0]
+      #stamp = final.draw(dx=Pixelsize)
 
       # Recenter the stamp at the desired position:
       stamp.setCenter(ix,iy)
@@ -189,6 +194,9 @@ def main(argv):
       bounds = stamp.bounds & full_image.bounds
       #print stamp.bounds, full_image.bounds, bounds
       full_image[bounds] += stamp[bounds]
+
+    # add sky background noise (does this add noise to the source too?)
+    #full_image.addNoise(galsim.PoissonNoise(rng, sky_level=Sky))
 
     # Write the file to disk:
     full_image.write(outfilename)
